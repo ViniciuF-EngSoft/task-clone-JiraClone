7274ffc76812740f697c4b41f5c90866613d5a8c
Vinicius Frota <viniciussantosfrota10012017@gmail.com>
2025-01-30 23:35:15 -0300

    implementing security and logout functionality
    added security functionality, and added a logout route using deletion of the logged in user's cookies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
+'use client'
 
+import { Button } from "@/components/ui/button";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter()
+  const { data, isLoading } = useCurrent()
+  const { mutate } = useLogout()
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push('/sign-in')
+    }
+  }, [data])
+
+
   return (
-    
     <div>
-      hello word
+      Visível somente para usuários autorizados
+      <Button onClick={() => mutate()}>
+        Sair
+      </Button>
     </div>
   );
 }
--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,13 +1,19 @@
 
 import { Hono } from "hono";
-import { createAdminClient } from "@/lib/appwrite";
-import { zValidator } from '@hono/zod-validator'
-import { loginSchema, registerSchema } from '../schema';
 import { ID } from "node-appwrite";
+import { zValidator } from '@hono/zod-validator'
 import { setCookie, deleteCookie } from 'hono/cookie'
+import { createAdminClient } from "@/lib/appwrite";
+import { sessionMiddleware } from "@/lib/session-middleware";
+import { loginSchema, registerSchema } from '../schema';
 import { AUTH_COOKIE } from "../constants";
 
 const app = new Hono()
+    .get('/current', sessionMiddleware, (c) => {
+        const user = c.get('user')
+
+        return c.json({ data: user })
+    })
     .post("/login", zValidator('json', loginSchema),
         async (c) => {
             const { email, password } = c.req.valid('json')
@@ -57,8 +63,13 @@ const app = new Hono()
             return c.json({ success: true })
         }
     )
-    .post('/logout', (c) => {
+    .post('/logout', sessionMiddleware, async (c) => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const account = c.get('account')
+
         deleteCookie(c, AUTH_COOKIE)
+        await account.deleteSession('current')
+
         return c.json({ success: true })
     })
 export default app
